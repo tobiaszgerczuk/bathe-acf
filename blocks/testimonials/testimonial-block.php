54ffd2fc7f20282f314f9dfe90bddacb8b7c8179
DESKTOP-H94GOJT\Tobiasz <firma.tobit@gmail.com>
2022-11-24 08:21:23 +0100

    initial

---
+++ blocks/testimonials/testimonial-block.php
@@ -0,0 +1,22 @@
+<?php 
+add_action('acf/init', 'my_acf_init_block_types');
+function my_acf_init_block_types() {
+    
+    // Check function exists.
+    if( function_exists('acf_register_block_type') ) {
+        
+        // register a testimonial block.
+        acf_register_block_type(array(
+            'name'              => 'testimonial',
+            'title'             => __('Testimonial'),
+            'description'       => __('A custom testimonial block.'),
+            'render_template'   => '\blocks\testimonials\testimonial-render.php',
+            'category'          => 'formatting',
+            'icon'              => 'admin-comments',
+            'keywords'          => array( 'testimonial', 'quote' ),
+        ));
+    }
+
+}
+
+?>
